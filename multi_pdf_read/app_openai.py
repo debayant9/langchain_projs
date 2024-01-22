3856e23277c937191bb069409bf3d6e5a1a5c360
indtal <Debayan.Talapatra@vanderlande.com>
2024-01-22 16:42:48 +0530

    Updtade recent changes

--- multi_pdf_read/app_openai.py
+++ multi_pdf_read/app_openai.py
@@ -1,23 +1,22 @@
 import streamlit as st
 import os
-import google.generativeai as genai
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-import google.generativeai as genai
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import PyPDFDirectoryLoader
 from langchain.prompts import PromptTemplate
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
 from dotenv import load_dotenv
 import io
-#import docx2txt
 import docx
 
 load_dotenv()
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+open_api_key = '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'
+os.environ["OPENAI_API_KEY"]=open_api_key
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -28,6 +27,7 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text+=page.extract_text()
     return text
+
 
 def get_doc_text(doc_files):
     text = ""
@@ -41,19 +41,21 @@ def get_doc_text(doc_files):
     return text
 
 
+
 def read_doc(directory):
     file_loader= PyPDFDirectoryLoader(directory)
     documents = file_loader.load()
     return documents
 
 
+
 def get_chunks(text):
-    text_splitter=RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=50)
     chunks=text_splitter.split_text(text)
     return chunks
 
 def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
     vector_stores=FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_stores.save_local("faiss_index")
 
@@ -75,21 +77,35 @@ def get_conversational_chain():
     return chain
 
 
-def user_input(user_question):
-    embeddings=GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+def user_input(vector_search_text):
+    embeddings=OpenAIEmbeddings(model="text-embedding-ada-002")
 
     new_db = FAISS.load_local("faiss_index", embeddings)
-    docs = new_db.similarity_search(user_question)
+    input_text = new_db.similarity_search(vector_search_text)
     #print(docs)
-    chain = get_conversational_chain()
+    return input_text
 
-    response = chain(
-        {"input_documents":docs, "question": user_question}
-        , return_only_outputs=True)
-    
+
+def openai_api_call(input_text, user_incident):
+    chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature = 0.5)
+
+    messages = [
+    SystemMessage(content="You are a Assistant to a human technician who is operating a baggage handling system in a huge airport.Your job is to \
+                  produce probable contingency steps, root causes of such incidents in the past and predict downtime of a component approximately based on such incidents which occured in the past. As refence, you will be given \
+                  a summary of past incidents which happened on the specific component, along with the contigency steps taken, root cause and downtime faced. The current incident also be given \
+                  as input by the human operator."),
+    HumanMessage(content=f'CONTEXT: {input_text}'),
+    HumanMessage(content=f'INCIDENT: {user_incident}'),
+    HumanMessage(content='As per the given context, what could be specific contingencies, root cause and downtime? Calculate the downtime based on the timestamps given. Be as specific as possible.'),
+    AIMessage(content='CONTINGENCY STEPS: '),
+    AIMessage(content='PROBABLE ROOT CAUSE: '),
+    AIMessage(content='DOWNTIME: '),
+    ]
+
+    chat(messages)
     #print(response)
-    st.write("Text to llm: ", docs)
-    st.write("Reply: ", response["output_text"])
+    st.write("Text to llm: ", input_text)
+    st.write("Reply: ", chat(messages).content)
 
 
 def main():
@@ -97,10 +113,14 @@ def main():
     st.header("AI Assistant")
 
     vec_search= st.text_input("Vector Search")
-    user_question = st.text_input("Component")
-
-    if user_question:
-        user_input(user_question)
+    user_incident = st.text_input("Incident")
+    user_incident12 = st.text_input("Incident12")
+ 
+    if st.button("Submit"):
+        if vec_search:
+            input_text = user_input(vec_search)
+            if user_incident:
+                openai_api_call(input_text, user_incident)
 
     with st.sidebar:
         st.title("Menu:")
@@ -116,3 +136,7 @@ def main():
     
 if __name__== "__main__":
     main()
+
+
+# 4650 unloader incidents summary, root cause, contingencies and downtime
+# 4650 unloader became unavailable 
